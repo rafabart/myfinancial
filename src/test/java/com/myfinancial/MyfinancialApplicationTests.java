b710fab4a70909a9e5e33e8be6c03d185160c28d
Rafael Marinho Cespedes <rafamola@gmail.com>
2019-11-07 16:51:47 -0300

    Project initial

---
+++ src/test/java/com/myfinancial/MyfinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfinancial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
